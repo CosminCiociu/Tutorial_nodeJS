6496ca5c225497dc7f44697c0aa2132620b76d37
CosminCiociu <tutu.cosmy@gmail.com>
2022-12-17 16:48:27 +0200

    Async functions

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-// npm - global command, comes with node
-// npm -- version
+const http = require('http')
 
-// local dependancy - use it only in particular project
-// npm i <packageName>
+const server = http.createServer((req,res) => {
 
-// global dependancy - use it in any project
-// npm install -g <packageName>
-// sudo install -g <packageName> 
+})
 
-// package.json - manifest file (store important info about project/package)
-// manual approach (create package.json in root, create proprties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
-
-const _ = require("lodash")
-
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-
-console.log(newItems);
+server.listen(5000, () => {
+    console.log('Server is listening on port 5000 ...')
+})
