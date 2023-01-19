aab5b4a3049cc9dc2a2e88a73a51f50126359d74
CosminCiociu <tutu.cosmy@gmail.com>
2023-01-19 12:16:42 +0200

    Read File Right Way

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req, res)=> {
-    server.on('request', (req, res) => {
-        res.end('welcome')
-    })
-})
-
-server.listen(5000);
